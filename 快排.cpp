cdac97e79fa34c27667c0ce3a9bce59ed7b1a5ed
wyc001208 <wyc20001208@126.com>
2020-11-19 21:41:04 +0800

    win20201119

--- 快排.cpp
+++ 快排.cpp
@@ -1,51 +1,68 @@
-#include<stdio.h>
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
-//划分算法部分，输入数组a、初始值、末端值，划分算法会将小于tmpt的数据放到tmpt左边，将大于tmpt的数据放到右边，并且返回一个位置信息i：
-int Partition(int a[], int s, int t)
-{
-    int i = s;
-    int j = t;
-    int tmpt = a[s];
-    //结束条件：i = j
-    while (i != j)
-    {
-        while (a[j] >= tmpt && j>i)//当i = j的时候就不能再让程序运行下去了。
-        {
-            j-- ;
-        }
-        a[i] = a[j];
-        while(i<j && a[i] <= tmpt)//道理同上。
-        {
-            i++;
-        }
-        a[j] = a[i];
-    }
-    a[i] = tmpt;//将tmpt定位到中间。
-    return i;
-}
+int Binary_search(int left, int right, int key);
 
-void QuickSort(int a[], int s, int t)
-{
-    if(s < t)//递归结束条件：s == t
-    {
-        int i = Partition(a, s, t);
-        QuickSort(a, s, i-1);
-        QuickSort(a, i+1, t);
-    }
-}
+vector<int> arr;
+vector<int> keys;
 
 int main()
 {
-    int a[] = {2,5,1,7,10,6,9,4,3,8};
-    QuickSort(a,0 ,9);
+    int n, m;
+	int temp = 0;
     int i = 0;
-    for (i = 0; i < 10; i++)
+    cin >> n;
+    for (i = 0; i < n; i++)
     {
-        cout << a[i] << ' ';
+        cin >> temp;
+        arr.push_back(temp);
+    }
+    //第三行输入m
+    cin >> m;
+    //接下来m行输入m个不同的数字
+    for (i = 0; i < m; i++)
+    {
+        cin >> temp;
+        keys.push_back(temp);
+    }
+    //二分查找
+    //每一行输出一个结果，Yes或者No
+    for (i = 0; i < m; i++)
+    {
+        if (Binary_search(0, n-1, keys[i]))
+        {
+            cout << "Yes" << endl;
+        }
+        else{
+            cout << "No" << endl;
+        }
     }
     return 0;
 }
 
+int Binary_search(int left, int right, int key)
+{
+	if (left > right)
+	{
+		return 0;
+	}
+    int mid = left+(right - left)/2;
+    if (arr[mid] == key || arr[left]==key||arr[right]==key )
+    {
+    	return 1;
+    }
+    else 
+    {
+    	if (key < arr[mid])
+    	{
+    		return Binary_search(left, mid - 1, key);
+    	}
+    	else
+    	{
+    		return Binary_search(mid + 1, right, key);
+    	}
+    }
+    return 0;
+}
